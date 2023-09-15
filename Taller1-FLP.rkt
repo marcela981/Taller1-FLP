323fb1c4844226908b779011df785e9e3c912c57
Hassen2208 <hassen.ortiz@correounivalle.edu.co>
2023-09-14 21:16:49 -0500

    Se agrega punto 3, 6 y 9

--- Taller1-FLP.rkt
+++ Taller1-FLP.rkt
@@ -27,6 +27,47 @@
 (newline)
 (newline)
 
+
+;; Punto 3
+;; list-set :
+;; Propósito:
+;; Procedimiento en el que una lista L, recibe un elemento x, se reemplaza en una posición n
+;; y devuelve una nueva lista similar a L pero con el elemento
+;; en la posición n (indexada desde cero) reemplazado por x.
+;;
+
+(define list-set
+  (lambda (l n x)
+    (letrec
+        (
+         (list-aux
+          (lambda (laux naux xaux acc)
+            (cond
+              [(eqv? laux '()) empty]
+              [(= acc naux) (cons xaux (list-aux (cdr laux) naux xaux (+ 1 acc)))]
+              [else (cons (car laux) (list-aux (cdr laux) naux xaux (+ 1 acc)))]
+              )
+            )
+          )
+         )
+      (list-aux l n x 0))
+    )
+  )
+;; Pruebas
+(display(list-set '(a b c d) 2 '(1 2)));; Retorna '(a b (1 2) d)
+(newline)
+
+(display(list-set '(a b c d) 3 '(1 5 10)));; Retorna '(a b c (1 5 10))
+(newline)
+
+(display(list-set '(a b c d) 1 '(1 2))); Retorna '(a '(1 2) c d)'
+(newline)
+
+(display(list-set '(apple banana cherry date) 2 '(x y z))) ; Retorna '(apple banana '(x y z) date)'
+(newline)
+
+
+
 ;Punto 4
 ;Se debe elaborar la función filter-in que recibe dos argumentos, predicado y lista
 ;La función retorna una lista que contiene los elementos que pertenecen a la lista y satisface el predicado
@@ -47,6 +88,34 @@
 (display (filter-in string? '(a b u (40 56) hi (a b c) (1 2 3))))
 (newline)
 (newline)
+
+
+
+;; Punto 6
+
+;; my-map :
+;; Propósito:
+;;   Aplica una función func a cada elemento de una lista lst y devuelve una nueva lista
+;;   que contiene los resultados de aplicar func a cada elemento de lst.
+(define (my-map func lst)
+  (if (null? lst)
+      '()
+      (cons (func (car lst))
+            (my-map func (cdr lst)))))
+
+;; swapper :
+;; Propósito:
+;; Intercambia las ocurrencias de dos elementos, E1 y E2, en una lista L.
+
+(define swapper
+  (lambda(E1 E2 L)
+  (define (swap x)
+    (cond
+      [(eq? x E1) E2]
+      [(eq? x E2) E1]
+      [else x]))
+
+  (my-map (lambda (x) (swap x)) L)))
 
 
 ;Punto 7
@@ -84,6 +153,42 @@
 (newline)
 (newline)
 
+
+
+;; Punto 9
+;; inversions :
+;; Propósito: Calcular el número de inversiones en una lista de números
+;; Una inversión es un par de elementos (i, j) en la lista donde i < j y L[i] > L[j]. En otras palabras, una inversión ocurre cuando un número en una posición anterior en la lista es mayor que un número en una posición posterior. La función utiliza un enfoque recursivo para contar las inversiones en la lista.
+;;.
+(define (inversions L)
+  (define inv-count 0)
+  
+  (define (count-inversions x xs)
+    (if (null? xs)
+        0
+        (+ (if (< x (car xs)) 0 1) (count-inversions x (cdr xs)))))
+    
+  (define (count-inversions-list L)
+    (if (null? L)
+        0
+        (+ (count-inversions (car L) (cdr L))
+           (count-inversions-list (cdr L)))))
+  
+  
+  (set! inv-count (count-inversions-list L))
+  
+ 
+  inv-count)
+
+;; Pruebas
+(display (inversions '(2 4 1 3 5)));; Retorna 3
+(newline)
+
+(display (inversions '(1 2 3 4 5))) ; Retorna 0
+(newline)
+
+(display (inversions '(3 1 3 2 1))) ; Retorna 8
+(newline)
 
 ;Punto 13
 ; Se elabora una función operate lrators lrands donde lrands es una lista de funciones binarias de tamaño n
